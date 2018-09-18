ab0d8b8b346b0a056da748089293bf8a1d00ca30
gayanch <agchamara93@gmail.com>
2018-09-18 12:02:43 +0530

    code cleanup

--- mbss-athenticator/components/com.wso2telco.mbss.authenticator/src/main/java/com/wso2telco/mbss/authenticator/util/TimeZoneUtils.java
+++ mbss-athenticator/components/com.wso2telco.mbss.authenticator/src/main/java/com/wso2telco/mbss/authenticator/util/TimeZoneUtils.java
@@ -2,40 +2,12 @@ package com.wso2telco.mbss.authenticator.util;
 
 import com.wso2telco.mbss.authenticator.model.TimeOffset;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.TimeZone;
-
 public class TimeZoneUtils {
-    /**
-     * Calculated the current time in provided timezone ID.
-     * @param timeZoneId Target Timezone ID.
-     * @return Current time in provided Timezone ID, null if timeZOneId is null.
-     */
-    public static Date currentTimeInTimeZone(String timeZoneId) {
-        if (timeZoneId == null) {
-            return null;
-        }
-
-        TimeZone timeZone = TimeZone.getTimeZone(timeZoneId);
-        SimpleDateFormat formatter = new SimpleDateFormat("HHmm");
-        formatter.setTimeZone(timeZone);
-
-        Date dateInTimeZone = null;
-        try {
-            dateInTimeZone = new SimpleDateFormat("HHmm").parse(formatter.format(new Date()));
-        } catch (ParseException e) {
-            e.printStackTrace();
-        }
-
-        return dateInTimeZone;
-    }
 
     /**
-     *
+     * Decodes time offset string into TimeOffset object.
      * @param offset time offset string in format -HH:MM, +HH:MM
-     * @return
+     * @return An instance of TimeOffset object with decoded data.
      */
     public static TimeOffset decodeOffsetString(String offset) {
         if (offset == null || offset.isEmpty()) {
