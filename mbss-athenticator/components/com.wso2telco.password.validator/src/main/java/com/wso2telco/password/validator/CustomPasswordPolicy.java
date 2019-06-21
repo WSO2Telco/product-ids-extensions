d78eb9f1d0e3c595efc08ee21bafb4468c1963e2
SkNuwanTissera <nuwan.tissera@my.sliit.lk>
2019-06-21 14:50:11 +0530

    INTGW-584 - mvn version set to 1.1.0-SNAPSHOT

--- mbss-athenticator/components/com.wso2telco.password.validator/src/main/java/com/wso2telco/password/validator/CustomPasswordPolicy.java
+++ mbss-athenticator/components/com.wso2telco.password.validator/src/main/java/com/wso2telco/password/validator/CustomPasswordPolicy.java
@@ -5,16 +5,21 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.wso2.carbon.identity.mgt.policy.AbstractPasswordPolicyEnforcer;
 
-
+/**
+ *  Custom validator for password policies
+ *  by extending the org.wso2.carbon.identity.mgt.policy.AbstractPasswordPolicyEnforcer abstract class.
+ *
+ *  Policy : Username cannot be a part of the password.
+ *
+ */
 public class CustomPasswordPolicy extends AbstractPasswordPolicyEnforcer {
   private static Log log = LogFactory.getLog(CustomPasswordPolicy.class);
   
   public CustomPasswordPolicy() {}
   
   public boolean enforce(Object... args) {
-    if (args != null)
+    if (args != null && args.length > 0)
     {
-
       String password = args[0].toString();
       String username = args[1].toString();
       if (password.toLowerCase().contains(username.toLowerCase()))
